a42af82178de3ed3fd7f01b1acb3d0df19d30501
DarshMohile <darshmohile303@gmail.com>
2025-08-06 23:53:15 +0530

    Updated Title and number format in UI

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ model = joblib.load("model.pkl")
 with open("selected_features.json", "r") as f:
     selected_features = json.load(f)
 
-st.title("Random Forest Regression Predictor")
+st.title("Prediction of Approximate Salary using Random Forest Regression Predictor")
 
 # User input form
 st.write("Enter input values for prediction:")
 user_input = {}
 for feature in selected_features:
-    user_input[feature] = st.number_input(f"{feature}", value=0.0)
+    user_input[feature] = st.number_input(f"{feature}", value=0)
 
 # Convert to DataFrame
 input_df = pd.DataFrame([user_input])
